32d4d6e6cea20ec1158646d73996b35c8b11d71a
Rhys Campbell <redstoneprofessional@gmail.com>
2025-08-22 00:45:40 +1000

    Updated vite config file

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,8 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="" element={<Home />} />
+          <Route path="portfolio/" element={<Home />} />
           <Route path="/skills" element={<Skills />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<About />} />
