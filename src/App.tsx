348ef56110c69f3b57d8ca00e5a6cf1982aaf09f
Rhys Campbell <redstoneprofessional@gmail.com>
2025-08-22 23:16:34 +1000

    Changed base URL

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import RenoCalculators from "./components/pages/projects/RenoCalculators";
 function App() {
   return (
     <div>
-      <Router>
+      <Router basename="import.meta.env.BASE_URL">
         <Navbar />
         <Routes>
           <Route path="" element={<Home />} />
